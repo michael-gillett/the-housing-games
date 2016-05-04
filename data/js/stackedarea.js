4923e2dc9c9b7fc56d1d65676d389c037948389f
Michael Gillett <mgillett54@gmail.com>
2016-05-04 16:15:38 -0400

    Each section of the stacked area is now clickable and the dorm table with scroll to and highlight the clicked dorm

--- data/js/stackedarea.js
+++ data/js/stackedarea.js
@@ -19,15 +19,15 @@
   }
 
   var width = 1000;
-  var height = 567;
-  var x_offset = 85;
-  var y_offset = 40;
+  var height = 650;
+  var x_offset = 90;
+  var y_offset = 50;
 
   var city_to_offset = {};
   var x_range = [y_offset];
   var y, x;
 
-  var file_name = 'data/stackedArea/2013_sa.csv';
+  var file_name = 'data/stackedArea/2012_sa.csv';
 
   // Create the svg chart that will house the visualization
   var svg = d3.select("#stackedarea").append("svg")
@@ -38,11 +38,14 @@
   // Create a y-scale for city distances
   var y = d3.scale.linear()
       .domain([1, 0])
-      .range([y_offset, height - y_offset]);
+      .range([y_offset, height - y_offset])
 
   var y_axis = d3.svg.axis()
-          .orient("left")
-          .scale(y);
+      .orient("left")
+      .scale(y)
+      .tickFormat(function(d, i) {
+        return (d * 100) + "%";
+      });
 
   // Create an x-scale for time
   var x = d3.scale.linear()
@@ -62,7 +65,7 @@
     .attr("class", "xlabel")
     .attr("text-anchor", "center")
     .attr("x", width / 2 - 75)
-    .attr("y", height - 2)
+    .attr("y", height - 10)
     .style("font-size","12pt")
     .text("Lottery Progress");
 
@@ -84,7 +87,7 @@
       .y0(function(d) { return y(d.y0); })
       .y1(function(d) { return y(d.y); });
 
-  var fundsChart = function(category, csvFile) {
+  var drawStackedArea = function(csvFile) {
     len_x = 0;
     dorms = []
     d3.csv(csvFile,
@@ -93,7 +96,7 @@
       if (i == 0) {
         Object.keys(d).forEach(function(key, i) {
           dorms.push([key, i])
-        })
+        });
       }
       result = [];
       result.push(i);
@@ -129,7 +132,7 @@
       // Draw the x axis
       svg.append('g')
          .attr('class', 'x_axis')
-         .attr("transform", "translate(-15," + (height - 36) + ")")
+         .attr("transform", "translate(-15," + (height - 45) + ")")
          .call(x_axis);
 
       // Draw the area sections
@@ -138,10 +141,9 @@
       for(i = 0; i < dorms.length; i++) {
         subdata = []
         data.forEach(function(d, j) {
-          subdata.push({"x": d[0], "y":(d[i+1] + prev_sum[j]) / (d[dorms.length + 1] + 0.001), "y0": ((prev_sum[j]) / (d[dorms.length + 1] + 0.001))})
+          subdata.push({"x": d[0], "y":(d[i+1] + prev_sum[j]) / (d[dorms.length + 1] + 0.001), "y0": ((prev_sum[j]) / (d[dorms.length + 1] + 0.001))});
           prev_sum[j] += d[i+1]
         });
-
         svg.append("path")
           .attr('class', 'area-'+i)
           .data([subdata])
@@ -151,93 +153,48 @@
       }
     });
   }
-  // Initially display the charr
-  fundsChart('all', file_name);
 
-  var lastColor = ""
-  var last = null;
-
-  $(document).on("click", "td", function() {
-    id = $(this).data("id");
-    $(this).addClass("highlight-row");
-
-    if (last != null) {
-      old_id = last.data("id");
-      $(".area-"+old_id).css("fill", lastColor);
-      last.removeClass("highlight-row");
-    }
-    lastColor = $(".area-"+id).css("fill");
-    last = $(this);
-
-    dorm = $(this).text();
-    $(".area-"+id).css("fill", "orange");
-
-
-  });
-
-  // $(document).on("mouseleave", "td", function() {
-  //   dorm = $(this).text();
-  //   id = $(this).data("id");
-  //   $(".area-"+id).css("fill", lastColor);
-  // });
 
   // Update the chart to display the new data
-  function updateData(dorm1, dorm2, csvFile) {
+  function updateData(csvFile) {
     len_x = 0
+    dorms = []
     d3.csv(csvFile,
     function(d, i) {
       len_x = (i + 1)
       if (i == 0) {
-        dorms = Object.keys(d)
+        Object.keys(d).forEach(function(key, i) {
+          dorms.push([key, i])
+        });
       }
       result = [];
       result.push(i);
       Object.keys(d).forEach(function(key) {
         result.push(+d[key]);
       });
+
+      total = result.slice(1,result.length).reduce(function(a,b) {return a + b});
+      result.push(total);
       return result;
     },
     function(error, data) {
-      console.log(data)
-      // Update the y axis
-      y.domain([d3.max(data, function(d) {
-        return Math.max(d[1], d[2]);
-      }), 0]);
+      svg.selectAll('path').remove();
+
+      // Dorm stuff
+      dorms.sort()
+      $("#available-dorms tbody tr").remove();
+      for(i = 0; i < dorms.length; i++) {
+        name = dorms[i][0];
+        index = dorms[i][1];
+        $("#available-dorms tbody").append("<tr><td data-id="+index+">"+name+"</td></tr>");
+      }
 
       x.domain([1, len_x]);
-      x_axis.tickValues([1, Math.floor(len_x*0.25), Math.floor(len_x*0.5), Math.floor(len_x*0.75), Math.floor(len_x*1.0)])
-      // svg.select('.x_axis').transition().duration(1000).call(x_axis);
+      x_axis.tickValues([1, Math.floor(len_x*0.25), Math.floor(len_x*0.5), Math.floor(len_x*0.75), Math.floor(len_x*1.0)]);
 
-      // svg.select('.x_axis').transition().duration(1000).call(x_axis);
       // Update the y-axis scale
       svg.select('.y_axis').transition().duration(1000).call(y_axis);
       svg.select('.x_axis').call(x_axis)
-
-      // Update the select drop downs
-      $('#select-first').find('option').remove().end()
-      $('#select-second').find('option').remove().end()
-      $.each(dorms, function(key, value) {
-        $('#select-first')
-         .append($("<option></option>")
-            .attr("value", value)
-            .text(value));
-        $('#select-second')
-         .append($("<option></option>")
-            .attr("value", value)
-            .text(value));
-      });
-      $('#select-first').val(dorm1)
-      $('#select-second').val(dorm2)
-
-      // Update the legend colors
-      // svg.select('.legend_view')
-      //   .transition().duration(1000)
-      //   .attr('fill-opacity', 0.7)
-      //   .attr('fill', category_to_color[category]);
-      // svg.select('.legend_fund')
-      //   .transition().duration(1000)
-      //   .attr('fill', category_to_color[category]);
-
 
       // Draw the area sections
       colors = lotsOfGrey(dorms.length)
@@ -245,13 +202,12 @@
       for(i = 0; i < dorms.length; i++) {
         subdata = []
         data.forEach(function(d, j) {
-          // console.log(prev_sum[j])
-          subdata.push({"x": d[0], "y":(d[i+1] + prev_sum[j]) / d[dorms.length + 1], "y0": ((prev_sum[j]) / d[dorms.length + 1])})
-          prev_sum[j] += d[i+1]
+          subdata.push({"x": d[0], "y":(d[i+1] + prev_sum[j]) / (d[dorms.length + 1] + 0.001), "y0": ((prev_sum[j]) / (d[dorms.length + 1] + 0.001))});
+          prev_sum[j] += d[i+1];
         });
 
         svg.append("path")
-          .attr('class', 'area')
+          .attr('class', 'area-'+i)
           .data([subdata])
           .attr("d", area)
           .style("fill", colors[i])
@@ -268,20 +224,44 @@
   //   update();
   // });
 
-  // $('#select-year').change(function() {
-  //   update();
-  // });
+  // Initially display the chart
+  drawStackedArea(file_name);
 
-  // function update() {
-  //   $('.vs').show()
-  //   dorm1 = $("#select-first").val();
-  //   dorm2 = $("#select-second").val();
-  //   year = $("#select-year").val();
-  //   $('.first').text(dorm1);
-  //   $('.second').text(dorm2);
-  //   $('#year').text(year)
+  var lastColor = ""
+  var last = null;
 
-  //   updateData(dorm1, dorm2, "data/"+year+"_sa.csv");
-  // }
+  $(document).on("click", "td", function() {
+    var id = $(this).data("id");
+    highlightDorm(id, $(this));
+  });
+
+  $('#area-year').change(function() {
+    lastColor = null;
+    var year = $(this).val()
+    updateData("data/stackedArea/"+year+"_sa.csv");
+  });
+
+  $(document).on("click", "[class^=area]", function() {
+    var id = $(this).attr('class').split('-')[1];
+    var row = $('*[data-id="'+id+'"]');
+    highlightDorm(id, row);
+  });
+
+  function highlightDorm(id, row) {
+    row[0].scrollIntoView(false);
+
+    row.addClass("highlight-row");
+
+    if (last != null) {
+      old_id = last.data("id");
+      $(".area-"+old_id).css("fill", lastColor);
+      last.removeClass("highlight-row");
+    }
+    lastColor = $(".area-"+id).css("fill");
+    last = row;
+
+    dorm = row.text();
+    $(".area-"+id).css("fill", "orange");
+  }
 
 })();
