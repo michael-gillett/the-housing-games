c9f86d2107513cdb20ba1671f3c0735c92c10498
Sam <sdunietz@gmail.com>
2016-05-13 19:00:35 -0500

    Redid linreg.py

--- code/Feature Learning/linreg.py
+++ code/Feature Learning/linreg.py
@@ -3,49 +3,47 @@ from sklearn.cross_validation import cross_val_score
 import numpy as np
 import csv
 
-dataFile = 'features.csv'
+def get_features(row):
+    return [float(f) for f in row[1:-1]]
+    #return [float(row[1])]
 
-features = []
-labels = []
+def extract(datafile):
+    features, labels = [], []
+    with open(datafile, 'r') as f:
+        reader = csv.reader(f)
+        reader.next()
+        for row in reader:
+            features.append(get_features(row))
+            labels.append(float(row[-1]))
+    return (features, labels)
 
-#collect data
-with open(dataFile, 'r') as f:
-	reader = csv.reader(f)
-	reader.next()
-	for row in reader:
-		float_features = []
-		float_features.append(1.0)#for bias term?
-		for feature in row[1:-1]:
-			float_features.append(float(feature))
-		features.append(float_features)
-		labels.append(float(row[-1]))
+def normalize(l):
+    mean = np.mean(l)
+    std = np.std(l)
+    if std == 0:
+        return [0 for i in xrange(len(l))]
+    else:
+        return [(n - mean) / std for n in l]
 
-def normalize(X):
-	#TODO: Fill in the code to normalize features!
-	#Z-score normalization
-	Xnorm = []
-	fMeans = np.mean(X, axis=0)
-	fStds = np.std(X, axis=0)
+def normalize_all(l):
+    if len(l) == 0:
+        return l
+    #Z-score normalization
+    # Split up features in X by component, normalize each, and then 
+    # Rejoin them together using zip function
+    separate_features = []
+    for i in xrange(len(l[0])):
+        feature = [e[i] for e in l]
+        normalized = normalize(feature)
+        separate_features.append(normalized)
+    return zip(*separate_features)
 
-	for obs in X:
-		obs_norm = []
-		for f in range(len(X[0])):
-			obs_norm.append((obs[f] - fMeans[f])/fStds[f])
-		Xnorm.append(obs_norm)
-	
-	return Xnorm
-
-normalize(features)
-
-# print features[0]
-# for row in features:
-# 	print features
-
+features, labels = extract('features.csv')
+features = normalize_all(features)
 model = LinearRegression()
 model.fit(features, labels)
-
-# print 'cross_val_score', np.mean(cross_val_score(model, features, labels, 'accuracy', 10))
-
+print 'cross_val_score', np.mean(cross_val_score(model, features, labels, scoring='mean_squared_error', cv=10))
 for i, coef in enumerate(model.coef_):
-	print i, coef
+    print i, coef
+
 
